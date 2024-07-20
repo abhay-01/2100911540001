558a02ac2a8fc6657f4ce3085ef7f85138cb31b3
abhay-01 <pratapsinghabhay999@gmail.com>
2024-07-20 11:39:46 +0530

    fixes

--- src/n-product/utils/service.js
+++ src/n-product/utils/service.js
@@ -5,7 +5,8 @@ export const fetchProducts = async (AMZ, Laptop, top, minPrice, maxPrice) => {
   try {
     const response = await axios.get(API_ENDPOINT.replace(':companies', AMZ).replace(':categories', Laptop), {
       params: { top, minPrice, maxPrice },
-      headers: { Authorization: `Bearer ${BEARER_TOKEN}` }
+      "Content-Type": "application/json",
+      headers: { Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzIxNDU1OTQ4LCJpYXQiOjE3MjE0NTU2NDgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImFhM2ZlNDVlLWI3ZTAtNGNkOS1iYjhlLTA0ZDRlMjM3YzQ5MCIsInN1YiI6IjIxY3NkczAxNkBqc3NhdGVuLmFjLmluIn0sImNvbXBhbnlOYW1lIjoiYWJoYXkiLCJjbGllbnRJRCI6ImFhM2ZlNDVlLWI3ZTAtNGNkOS1iYjhlLTA0ZDRlMjM3YzQ5MCIsImNsaWVudFNlY3JldCI6ImdtY0RySXFjbXVwZVFyWGciLCJvd25lck5hbWUiOiJhYmhheSIsIm93bmVyRW1haWwiOiIyMWNzZHMwMTZAanNzYXRlbi5hYy5pbiIsInJvbGxObyI6IjIxMDA5MTE1NDAwMDEifQ.gMZ_4HGfGjY2yORLsNcEAMI5a99aIIIj0PZj-yqy1wA'}` }
     });
 
     console.log('Fetched products:', response.data);
